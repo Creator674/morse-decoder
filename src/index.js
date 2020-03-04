b958fdccb3825b184dba15545af1bcd19184f51f
YaroslavR <666anastyalena@mail.ru>
2020-03-04 21:29:40 +0300

    feat:task complete

--- src/index.js
+++ src/index.js
@@ -38,7 +38,44 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+let string='';
+for(let i = 0; i < expr.length; i++){
+    if (i%10==0 && i!=0){
+        string+=' ';
+    }
+
+    if(expr[i]=='*' && expr[i+9]=='*'){
+        string+="*";
+        i+=9;
+        continue;
+    }
+    else if(expr[i]=='1' && expr[i+1]=='1'){
+        string+='-';
+        i++;
+        continue;
+    }
+    else if(expr[i]=='1' && expr[i+1]=='0'){
+        string+='.';
+        i++;
+        continue;
+    }
+    else if(expr[i] == '0'){
+        string+='';
+    }
+}
+
+let answer = '';
+let arr = string.split(' ');
+
+for(let i=0;i<arr.length;i++){
+    for(var key in MORSE_TABLE){
+        if(arr[i] == key){
+            answer+=MORSE_TABLE[key];
+        }
+    }
+    if(arr[i] == "*"){answer+=" ";};
+}
+return answer;
 }
 
 module.exports = {
